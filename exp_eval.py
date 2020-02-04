da3b4eaf8beb9071e3ce7b3479f919d6232ec2f6
Israel <izrael.banez@gmail.com>
2020-02-03 19:11:52 -0800

    I added the code for exp_eval and tests

--- exp_eval.py
+++ exp_eval.py
@@ -11,7 +11,48 @@ def postfix_eval(input_str):
     are space separated.  Tokens are either operators + - * / ** << >> or numbers (integers or floats)
     Returns the result of the expression evaluation. 
     Raises an PostfixFormatException if the input is not well-formed"""
-    pass
+    stack_hold = Stack(30)
+    operator = ['+', '-', '/', '*', '>>', '<<', '**']
+    tokens = input_str.split()
+    if len(input_str) == 0:
+        raise PostfixFormatException('Insufficient operands')
+    for i in tokens:
+        if i in operator:
+            if stack_hold.size() < 2:
+                raise PostfixFormatException('Insufficient operands')
+            num2 = stack_hold.pop()
+            num1 = stack_hold.pop()
+            if i == '+':
+                stack_hold.push(float(num1) + float(num2))
+            elif i == '-':
+                stack_hold.push(float(num1) - float(num2))
+            elif i == '*':
+                stack_hold.push(float(num1) * float(num2))
+            elif i == '/':
+                try:
+                    stack_hold.push(float(num1) / float(num2))
+                except ValueError:
+                    raise ZeroDivisionError
+            elif i == '**':
+                stack_hold.push(float(num1) ** float(num2))
+            elif i == '<<':
+                try:
+                    stack_hold.push(int(num1) << int(num2))
+                except ValueError:
+                    raise PostfixFormatException('Illegal bit shift operand')
+            elif i == '>>':
+                try:
+                    stack_hold.push(int(num1) >> int(num2))
+                except ValueError:
+                    raise PostfixFormatException('Illegal bit shift operand')
+        elif i not in operator:
+            try:
+                stack_hold.push(float(i))
+            except ValueError:
+                raise PostfixFormatException('Invalid token')
+    if stack_hold.size() > 1:
+        raise PostfixFormatException('Too many operands')
+    return stack_hold.pop()
 
 def infix_to_postfix(input_str):
     """Converts an infix expression to an equivalent postfix expression"""
@@ -19,14 +60,93 @@ def infix_to_postfix(input_str):
     """Input argument:  a string containing an infix expression where tokens are 
     space separated.  Tokens are either operators + - * / ** << >> or numbers (integers or floats)
     Returns a String containing a postfix expression """
-    pass
-
+    stack_hold = Stack(30)
+    tokens = input_str.split()
+    precedence = {
+        '(': 5,
+        ')': 5,
+        '+': 4,
+        '-': 4,
+        '*': 3,
+        '/': 3,
+        '**': 2,
+        '<<': 1,
+        '>>': 1}
+    stack = []
+    if len(input_str) == 0:
+        return ''
+    for i in range(len(tokens)):
+        if numeric(tokens[i]) is True:
+            stack.append(tokens[i])
+        elif tokens[i] == '(':
+            stack_hold.push(tokens[i])
+        elif tokens[i] == ')':
+            while stack_hold.peek() != '(':
+                stack.append(stack_hold.pop())
+            stack_hold.pop()
+        elif precedence[tokens[i]] == 2:
+            for k in range(stack_hold.size()):
+                if precedence[stack_hold.items[k]] < precedence[tokens[i]]:
+                    stack.append(stack_hold.pop())
+                else:
+                    break
+            stack_hold.push(tokens[i])
+        elif tokens[i] in precedence:
+            for j in range(stack_hold.size(), 0, -1):
+                if precedence[stack_hold.items[j - 1]] <= precedence[tokens[i]]:
+                    stack.append(stack_hold.pop())
+                else:
+                    break
+            stack_hold.push(tokens[i])
+    while stack_hold.size() > 0:
+        stack.append(stack_hold.pop())
+    return ' '.join(stack)
 
 def prefix_to_postfix(input_str):
     """Converts a prefix expression to an equivalent postfix expression"""
     """Input argument: a string containing a prefix expression where tokens are 
     space separated.  Tokens are either operators + - * / ** << >> or numbers (integers or floats)
     Returns a String containing a postfix expression(tokens are space separated)"""
-    pass
+    stack_hold = Stack(30)
+    tokens = input_str.split()
+    if len(input_str) == 0:
+        return ''
+    for i in range(len(tokens), 0, -1):
+        if numeric(tokens[i - 1]) is False:
+            first = stack_hold.pop()
+            second = stack_hold.pop()
+            ad = first + ' ' + second + ' ' + tokens[i - 1]
+            stack_hold.push(ad)
+        else:
+            stack_hold.push(tokens[i - 1])
+    return stack_hold.pop()
 
+def numeric(input):
+    '''Checks for real numbers including floats and negatives'''
+    try:
+        float(input)
+        return True
+    except ValueError:
+        return False
 
+if __name__ == "__main__":
+
+    a = '1 + 1 - 2'
+    b = infix_to_postfix(a)
+    print(b)
+
+    c = '5.5 5 6.6 * +'
+    d = postfix_eval(c)
+    print(d)
+
+    e = '- / 30 6 4'
+    f = prefix_to_postfix(e)
+    print(f)
+
+    g = '-25.6'
+    h = numeric(g)
+    print(h)
+
+    i = 'abc'
+    j = numeric(i)
+    print(j)
--- exp_eval_tests.py
+++ exp_eval_tests.py
@@ -5,73 +5,158 @@ from exp_eval import *
 
 class test_expressions(unittest.TestCase):
     def test_postfix_eval_01a(self):
+        '''tests for value of postfix equation of +'''
         self.assertAlmostEqual(postfix_eval("3 5 +"), 8)
 
+    def test_postfix_eval_01ab(self):
+        '''tests for value of postfix equation of -'''
+        self.assertAlmostEqual(postfix_eval("11 5.5 -"), 5.5)
+
+    def test_postfix_eval_01ac(self):
+        '''tests for ZeroDivisionError'''
+        g = "0 0 /"
+        with self.assertRaises(ZeroDivisionError):
+            postfix_eval(g)
+
+        f = '1 0 /'
+        with self.assertRaises(ZeroDivisionError):
+            postfix_eval(f)
+
     def test_postfix_eval_01b(self):
+        '''tests postfix for >> and <<'''
         self.assertAlmostEqual(postfix_eval("8 1 >>"), 4)
         self.assertAlmostEqual(postfix_eval("8 1 <<"), 16)
 
     def test_postfix_eval_01c(self):
+        '''tests postfix for **'''
         self.assertAlmostEqual(postfix_eval("8 2 **"), 64)
 
     def test_postfix_eval_01d(self):
+        '''tests postfix for a combo of operands'''
         self.assertAlmostEqual(postfix_eval("18.5 -2 *"), -37)
         self.assertAlmostEqual(postfix_eval("-18.52 -0.78 +"), -19.3)
 
     def test_postfix_eval_02(self):
+        '''tests for Invalid tokens'''
         try:
             postfix_eval("blah")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Invalid token")
 
+        g = 'abc'
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval(g)
+
     def test_postfix_eval_03a(self):
+        '''tests for insufficient operands'''
         try:
             postfix_eval("4 +")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Insufficient operands")
 
+        g = '2 -'
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval(g)
+
+
     def test_postfix_eval_03b(self):
+        '''tests for empty operands'''
         try:
-            postfix_eval("")
+            postfix_eval('')
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Insufficient operands")
 
+        g = ''
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval(g)
+
+
     def test_postfix_eval_04(self):
+        '''tests for too many operands'''
         try:
             postfix_eval("1 2 3 +")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Too many operands")
 
+        g = '3 4 5 *'
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval(g)
+
+    def test_postfix_eval_05(self):
+        '''tests for illegal bit shift operands of <<'''
+        try:
+            postfix_eval("123.42 -232.23 <<")
+            self.fail()
+        except PostfixFormatException as e:
+            self.assertEqual(str(e), "Illegal bit shift operand")
+
+        g = "123.42 -232.23  <<"
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval(g)
+
+    def test_postfix_eval_06(self):
+        '''tests for illegal bit shift operands of >>'''
+        try:
+            postfix_eval("123.42 -232.23 >>")
+            self.fail()
+        except PostfixFormatException as e:
+            self.assertEqual(str(e), "Illegal bit shift operand")
+
+        g = "123.42 -232.23  >>"
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval(g)
+
+
     def test_infix_to_postfix_01a(self):
+        '''tests for infix converstion to postfix'''
         self.assertEqual(infix_to_postfix("6 - 3"), "6 3 -")
         self.assertEqual(infix_to_postfix("6"), "6")
         self.assertEqual(infix_to_postfix("32 >> 2 >> 1"), "32 2 >> 1 >>")
+        self.assertEqual(infix_to_postfix(''), '')
 
     def test_infix_to_postfix_01b(self):
+        '''tests for infix converstion to postfix'''
         self.assertEqual(infix_to_postfix("32 >> 2 << 1"), "32 2 >> 1 <<")
 
     def test_infix_to_postfix_01c(self):
+        '''tests for right associativity'''
         self.assertEqual(infix_to_postfix("3 ** 2 ** 2"), "3 2 2 ** **")
 
     def test_infix_to_postfix_02(self):
+        '''tests for infix converstion to postfix for a combo of operands'''
         self.assertEqual(infix_to_postfix("( 5 - 3 ) * 4"), "5 3 - 4 *")
         self.assertEqual(infix_to_postfix("3 + 4 * 2 / ( 1 - 5 ) ** 2 ** 3"), "3 4 2 * 1 5 - 2 3 ** ** / +")
 
     def test_infix_to_postfix_03(self):
+        '''tests for infix converstion to postfix when two operands are close'''
         self.assertEqual(infix_to_postfix("70 - -3 * 10"), "70 -3 10 * -")
 
     def test_infix_to_postfix_04(self):
+        '''tests for infix converstion to postfix'''
         self.assertEqual(infix_to_postfix("70.52 - 3.5 * 10.05"), "70.52 3.5 10.05 * -")
 
     def test_infix_to_postfix_05(self):
+        '''tests for infix converstion to postfix' of negatives'''
         self.assertEqual(infix_to_postfix("-70.52 - 3.5 * 10.05"), "-70.52 3.5 10.05 * -")
 
     def test_prefix_to_postfix(self):
+        '''tests for prefix converstion to postfix'''
         self.assertEqual(prefix_to_postfix("* - 3 / 2 1 - / 4 5 6"), "3 2 1 / - 4 5 / 6 - *")
+        self.assertEqual(prefix_to_postfix(""), "")
+
+    def test_prefix_to_postfix_a(self):
+        '''tests for prefix converstion to postfix'''
+        self.assertEqual(prefix_to_postfix("* - 3 2 1"), "3 2 - 1 *")
+
+    def test_numeric(self):
+        '''tests for numbers that include negatives, positives, and floats'''
+        self.assertFalse(numeric('gg ez'), False)
+        self.assertTrue(numeric('805'), True)
+
 
 if __name__ == "__main__":
     unittest.main()
